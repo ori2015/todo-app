d9780d32d64440ae7543bfd706d8fc7193921e08
ori <ori1598@gmail.com>
2025-08-05 23:18:42 +0300

    Update project to .NET 8.0 and add .gitignore file

--- api/Migrations/TodoDbModelSnapshot.cs
+++ api/Migrations/TodoDbModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace TodoApp.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.0")
+                .HasAnnotation("ProductVersion", "8.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
             modelBuilder.Entity("TodoApp.Models.TodoItem", b =>
